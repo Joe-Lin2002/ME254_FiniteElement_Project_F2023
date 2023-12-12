437afe92d401454494c01e57663aafa5f7a60ff7
Junyi Lin <junyi.lin@rochester.edu>
2023-12-11 20:22:08 -0500

    Update Q4_shape_functions.m

--- Q4_shape_functions.m
+++ Q4_shape_functions.m
@@ -9,5 +9,4 @@ function [N, dNdxi] = Q4_shape_functions(xi, eta)
     % Shape functions derivatives with respect to xi and eta
     dNdxi = 1/4 * [-1+eta, 1-eta, 1+eta, -1-eta;  % dN/dxi
                    -1+xi, -1-xi,  1+xi, 1-xi];    % dN/deta
-
 end
