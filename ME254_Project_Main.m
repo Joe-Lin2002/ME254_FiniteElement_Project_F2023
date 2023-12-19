49f601b37ce68f2b054756a6bb39a28336f761d2
Junyi Lin <junyi.lin@rochester.edu>
2023-12-19 16:17:51 -0500

    stiffness_cal finish

--- ME254_Project_Main.m
+++ ME254_Project_Main.m
@@ -9,3 +9,7 @@ close all;
 directory = 'Project-files/input_coarse_mesh/'; % Input files directory
 data = read_input(directory); % Input Reading
 
+for i = 1:size(data.elemconn,1)
+    stiff{i} = stiffness_cal([data.coord(data.elemconn(i,1:4),1),data.coord(data.elemconn(i,1:4),2)], ...
+        data.matprop);
+end
--- stiffness_cal.m
+++ stiffness_cal.m
@@ -1,3 +1,61 @@
-function [outputArg1,outputArg2] = stiffness_cal(inputArg1,inputArg2)
+function K = stiffness_cal(coordinates, matprop)
+%% Connectivity matrix
+elements = [1, 2, 3, 4];
 
+t = matprop(1);
+v = matprop(2);
+E = matprop(3);
+
+%% Define Gauss quadrature points and weights for full integration
+gauss_points = [-1/sqrt(3), -1/sqrt(3);
+    1/sqrt(3), -1/sqrt(3);
+    1/sqrt(3),  1/sqrt(3);
+    -1/sqrt(3),  1/sqrt(3)];
+weights = [1, 1, 1, 1];
+
+%% Initialize global stiffness matrix
+K = zeros(8, 8);
+
+%% Assemble stiffness matrix
+for i = 1:size(elements, 1)
+    % Extract nodal coordinates for the current element
+    % Extract nodal coordinates for the current element
+    element_nodes = elements(i, :);
+    node_coords = coordinates(element_nodes, :);
+
+    % Perform numerical integration using Gauss quadrature
+    for gp = 1:length(weights)
+        xi = gauss_points(gp, 1);
+        eta = gauss_points(gp, 2);
+        w = weights(gp);
+
+        % Compute shape functions and their derivatives
+        [N, dNdxi] = Q4_shape_functions(xi, eta);
+
+        % Compute Jacobian matrix and its determinant
+        % Here, dNdxi is a 2x4 matrix, and node_coords is a 4x2 matrix
+        J = dNdxi * node_coords;  % Transpose dNdxi to get a 4x2 matrix
+        detJ = det(J);
+
+        % Construct B matrix (strain-displacement matrix)
+        alpha = [1,0,0,0;
+                0,0,0,1;
+                0,1,1,0];
+        beta = [J^-1,zeros(2,2);zeros(2,2),J^-1];
+        gamma = [dNdxi(1,1),0,dNdxi(1,2),0,dNdxi(1,3),0,dNdxi(1,4),0;
+                dNdxi(2,1),0,dNdxi(2,2),0,dNdxi(2,3),0,dNdxi(2,4),0;
+                0,dNdxi(1,1),0,dNdxi(1,2),0,dNdxi(1,3),0,dNdxi(1,4);
+                0,dNdxi(2,1),0,dNdxi(2,2),0,dNdxi(2,3),0,dNdxi(2,4);];
+        B = alpha*beta*gamma;
+
+        % Material matrix
+        D = (E/(1-v^2)) * [1, v, 0;
+            v, 1, 0;
+            0, 0, (1-v)/2];
+
+        % Compute element stiffness matrix
+        I = B' * D * B * w * detJ * t;
+        K = K + I;
+    end
+end
 end
