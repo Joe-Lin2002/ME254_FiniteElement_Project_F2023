560822334d78c325ded3b6e49714e73f761a6a2c
Cadybru <cbrunecz@u.rochester.edu>
2023-12-19 16:08:51 -0500

    Update PlaneStressStrain.m

--- PlaneStressStrain.m
+++ PlaneStressStrain.m
@@ -1,6 +1,19 @@
-function [outputArg1,outputArg2] = untitled(inputArg1,inputArg2)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+% Determine which conditions using
+function [E,t] = PlaneStessStrain(matprop)
+%Plane Stress or Strain
+planechoice = input('Enter 1 for plane stress, 2 for plane strain: ');
+if planechoice==1
+t=matprop(1);
+nu = matprop(2);
+E = matprop(3)/(1-nu^2).*[1 nu 0
+                          nu 1 0
+                          0 0 (1-nu)/2];
+else
+t=1;
+nu = matprop(2);
+E = matprop(3)/((1+nu)*(1-2*nu)).*[1-nu nu 0
+                                   nu 1-nu 0
+                                   0 0 1-2*nu];
+end
+
 end
