5504f290bc0f1f473523a745a572b155f4110e1e
Cadybru <cbrunecz@u.rochester.edu>
2023-12-19 15:04:24 -0500

    Update ME254_Project_Main.m

--- ME254_Project_Main.m
+++ ME254_Project_Main.m
@@ -2,8 +2,11 @@
 %% ME254_Project_Main, Cady Brunecz, Junyi Lin, Dec11 2023
 
 % Constants
+
 E = 1e9; % Pa
 nu = 0.3; % Poisson ratio
 P = 1; % KN
 
-% Read Data
+% Read data in
+readmatrix('bc_code.txt') % Replace with boundary conditions
+readmatrix('coord.txt') % Replace with nodal coords
