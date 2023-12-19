4a5db2706ec0138e4dc62aec1a696ad5557c1e46
Cadybru <cbrunecz@u.rochester.edu>
2023-12-19 16:04:50 -0500

    Update plane_main.m

--- Project-files/plane_main.m
+++ Project-files/plane_main.m
@@ -7,17 +7,23 @@ load loads.txt
 load matprop.txt
 
 %Plane Stress or Strain
-%planechoice = input('Enter 1 for plane stress, 2 for plane strain: ');
-% Thickness does not vary, so t = t.
-% For plane strain, t=1
-%if planechoice==1
-%t=matprop(1);
-%else
-%t=1
-%end
+planechoice = input('Enter 1 for plane stress, 2 for plane strain: ');
+if planechoice==1
+t=matprop(1);
+nu = matprop(2);
+E = matprop(3)/(1-nu^2).*[1 nu 0
+                          nu 1 0
+                          0 0 (1-nu)/2];
+else
+t=1;
+nu = matprop(2);
+E = matprop(3)/((1+nu)*(1-2*nu)).*[1-nu nu 0
+                                   nu 1-nu 0
+                                   0 0 1-2*nu];
+end
 
 %Full or reduced integration
-%intchoice = input('Enter 1 for reduced integration, 2 for full integration: ');
+intchoice = input('Enter 1 for reduced integration, 2 for full integration: ');
 
 % determine work size
 NumNodes=size(coord,1);
