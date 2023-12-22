6f36659d94f5a080aa0b6d425bf4fc6fbfed7b69
Cadybru <cbrunecz@u.rochester.edu>
2023-12-22 16:09:47 -0500

    Update PlaneStressStrain.m

--- PlaneStressStrain.m
+++ PlaneStressStrain.m
@@ -1,17 +1,24 @@
 % Determine which conditions using
 function D = PlaneStessStrain(matprop, plane_flag)
 %Plane Stress or Strain
-planechoice = plane_flag;
-if planechoice==1
     nu = matprop(2);
-    D = matprop(3)/(1-nu^2).*[1 nu 0
-        nu 1 0
-        0 0 (1-nu)/2];
-else
-    nu = matprop(2);
-    D = matprop(3)/((1+nu)*(1-2*nu)).*[1-nu nu 0
-        nu 1-nu 0
-        0 0 1-2*nu];
+    E = matprop(3);
+    
+    if flag == 1  % Reduced integration
+        nu_r = 2/3 * nu;
+    else
+        nu_r = nu;
+    end
+
+    if plane_flag == 1  % Plane stress
+        D = E / (1 - nu_r^2) * [1, nu_r, 0;
+                                nu_r, 1, 0;
+                                0, 0, (1 - nu_r) / 2];
+    else  % Plane strain
+        D = E / ((1 + nu_r) * (1 - 2 * nu_r)) * [1 - nu_r, nu_r, 0;
+                                                  nu_r, 1 - nu_r, 0;
+                                                  0, 0, (1 - 2 * nu_r) / 2];
+    end
 end
 
-end
+
